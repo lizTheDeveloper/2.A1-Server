ea0a33a1d8779c0da2748942485266731fa6df17
Liz Howard <liz@codepath.org>
2020-07-25 21:48:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def server_time_check(task_config, task_times):
+  return 0
+  
+
+## Please do not change the code below this line
+## These lines are how the tests interact with the code
+
+task_config = input("Please input the number of tasks, and the maximum total execution time:")
+task_times = input("Please input the execution times of each task, seperated with a space:")
+
+server_time_check(task_config, task_times)
